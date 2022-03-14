26ca4d72b5cce4d74f79836851f15f9c5513203d
damon-luong <78005335+damon-luong@users.noreply.github.com>
2022-03-14 17:42:35 -0400

    updated app.js
    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Item from './buyer/components/Item.js';
+import Checkout from './buyer/components/Checkout.js';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Item />
+      <Checkout />
     </div>
   );
 }

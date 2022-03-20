ec2d81a17a370c157dfc0a036f07e0cd646c3fa5
cindych <78625079+cindych@users.noreply.github.com>
2022-03-20 03:48:23 -0400

    fixed eslint msgs

--- src/homepage/components/Homepage.js
+++ src/homepage/components/Homepage.js
@@ -3,25 +3,24 @@ import {
   CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, Image,
 } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
-import { Button } from 'react-bootstrap';
+import { v4 as uuidv4 } from 'uuid';
 import links from '../assets/productimages.json';
 import '../styles/Homepage.css';
 
-const Homepage = () => {
-  return (
-    <div className="homepage" style={{ paddingTop: '150px' }}>
-      <div className="carousel-wrapper">
-        <h1 className>Today's listings</h1>
-        <CarouselProvider
-          className="mt-1"
-          naturalSlideWidth={100}
-          naturalSlideHeight={100}
-          totalSlides={links.length}
-          visibleSlides={5}
-        >
+const Homepage = () => (
+  <div className="homepage" style={{ paddingTop: '150px' }}>
+    <div className="carousel-wrapper">
+      <h1>Today&apos;s listings</h1>
+      <CarouselProvider
+        className="mt-1"
+        naturalSlideWidth={100}
+        naturalSlideHeight={100}
+        totalSlides={links.length}
+        visibleSlides={5}
+      >
         <Slider>
           {links.map((link) => (
-            <Slide>
+            <Slide key={uuidv4()}>
               <Image>
                 <img src={link} alt="product pic" width="100%" height="100%" />
               </Image>
@@ -33,62 +32,61 @@ const Homepage = () => {
           <ButtonBack className="btn me-2">Back</ButtonBack>
           <ButtonNext className="btn">Next</ButtonNext>
         </div>
-        </CarouselProvider>
-      </div>
-
-      <div className="carousel-wrapper">
-        <h1 className>Trending Listings</h1>
-        <CarouselProvider
-          className="mt-1"
-          naturalSlideWidth={100}
-          naturalSlideHeight={100}
-          totalSlides={links.length}
-          visibleSlides={5}
-        >
-        <Slider>
-          {links.map((link) => (
-            <Slide>
-              <Image>
-                <img src={link} alt="product pic" width="100%" height="100%" />
-              </Image>
-              <p>$5.99</p>
-            </Slide>
-          ))}
-        </Slider>
-        <div className="buttons">
-          <ButtonBack className="btn me-2">Back</ButtonBack>
-          <ButtonNext className="btn">Next</ButtonNext>
-        </div>
-        </CarouselProvider>
-      </div>
-
-      <div className="carousel-wrapper">
-        <h1 className>Saved Listings</h1>
-        <CarouselProvider
-          className="mt-1"
-          naturalSlideWidth={100}
-          naturalSlideHeight={100}
-          totalSlides={links.length}
-          visibleSlides={5}
-        >
-        <Slider>
-          {links.map((link) => (
-            <Slide>
-              <Image>
-                <img src={link} alt="product pic" width="100%" height="100%" />
-              </Image>
-              <p>$5.99</p>
-            </Slide>
-          ))}
-        </Slider>
-        <div className="buttons">
-          <ButtonBack className="btn me-2">Back</ButtonBack>
-          <ButtonNext className="btn">Next</ButtonNext>
-        </div>
-        </CarouselProvider>
-      </div>
+      </CarouselProvider>
     </div>
-  );
-};
+
+    <div className="carousel-wrapper">
+      <h1>Trending Listings</h1>
+      <CarouselProvider
+        className="mt-1"
+        naturalSlideWidth={100}
+        naturalSlideHeight={100}
+        totalSlides={links.length}
+        visibleSlides={5}
+      >
+        <Slider>
+          {links.map((link) => (
+            <Slide key={uuidv4()}>
+              <Image>
+                <img src={link} alt="product pic" width="100%" height="100%" />
+              </Image>
+              <p>$5.99</p>
+            </Slide>
+          ))}
+        </Slider>
+        <div className="buttons">
+          <ButtonBack className="btn me-2">Back</ButtonBack>
+          <ButtonNext className="btn">Next</ButtonNext>
+        </div>
+      </CarouselProvider>
+    </div>
+
+    <div className="carousel-wrapper">
+      <h1>Saved Listings</h1>
+      <CarouselProvider
+        className="mt-1"
+        naturalSlideWidth={100}
+        naturalSlideHeight={100}
+        totalSlides={links.length}
+        visibleSlides={5}
+      >
+        <Slider>
+          {links.map((link) => (
+            <Slide key={uuidv4()}>
+              <Image>
+                <img src={link} alt="product pic" width="100%" height="100%" />
+              </Image>
+              <p>$5.99</p>
+            </Slide>
+          ))}
+        </Slider>
+        <div className="buttons">
+          <ButtonBack className="btn me-2">Back</ButtonBack>
+          <ButtonNext className="btn">Next</ButtonNext>
+        </div>
+      </CarouselProvider>
+    </div>
+  </div>
+);
 
 export default Homepage;

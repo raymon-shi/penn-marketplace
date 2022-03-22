383a20cb6f85615edb73e970208ac7cc3732345e
Raymon Shi <61591942+raymon-shi@users.noreply.github.com>
2022-03-21 20:24:38 -0400

    removed container for login form

--- src/Login/components/LoginForm.js
+++ src/Login/components/LoginForm.js
@@ -11,25 +11,23 @@ const LoginForm = () => {
 
   return (
     <div>
-      <Container className="shadow p-0 mb-5 rounded" style={{ marginRight: '18rem' }}>
-        <Form className="card p-3 bg-light">
-          <Form.Group className="mb-3" controlId="loginFormPennEmail">
-            <Form.Control type="email" placeholder="Penn Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-          </Form.Group>
-          <Form.Group className="mb-4" controlId="loginFormPassword">
-            <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-          </Form.Group>
-          <Form.Group style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <Button variant="primary" className="w-75">
-              Login
-            </Button>
-            <hr />
-            <Button variant="danger" className="w-50" onClick={() => setShowSignUp(true)}>
-              Sign Up
-            </Button>
-          </Form.Group>
-        </Form>
-      </Container>
+      <Form className="card p-3 bg-light" style={{ justifyContent: 'center' }}>
+        <Form.Group className="mb-3" controlId="loginFormPennEmail">
+          <Form.Control type="email" placeholder="Penn Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        </Form.Group>
+        <Form.Group className="mb-4" controlId="loginFormPassword">
+          <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        </Form.Group>
+        <Form.Group style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <Button variant="primary" className="w-75">
+            Login
+          </Button>
+          <hr />
+          <Button variant="danger" className="w-50" onClick={() => setShowSignUp(true)}>
+            Sign Up
+          </Button>
+        </Form.Group>
+      </Form>
       {showSignUp ? <SignUpForm showSignUp={showSignUp} setShowSignUp={setShowSignUp} /> : ''}
     </div>
   );

12c0f67d8bc146a84dca7fead72b6abcff887417
hly8 <98903564+hly8@users.noreply.github.com>
2022-04-25 02:07:04 -0400

    Modified search to look for bid and regular listings

--- backend/routes/item.js
+++ backend/routes/item.js
@@ -27,6 +27,16 @@ router.post('/search',  async (req, res, next) => {
   }
 });
 
+router.post('/bidSearch',  async (req, res, next) => {
+  const pattern = new RegExp(`${req.body.filter}`, 'i');
+  try {
+    const bidListings = await ItemBid.find({ itemName: pattern });
+    res.json(bidListings);
+  } catch (error) {
+    next(new Error('Error with retrieving search results'));
+  }
+});
+
 // route to retrieve regular listings
 router.get('/getRegListings', async (req, res, next) => {
   try {
--- frontend/src/searchbar/components/SearchResults.js
+++ frontend/src/searchbar/components/SearchResults.js
@@ -1,14 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Col, Row } from 'react-bootstrap';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import {
+  CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, Image,
+} from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+import { v4 as uuidv4 } from 'uuid';
 import ResultCard from './ResultCard';
-import SearchPagination from './SearchPagination';
 
 const SearchResults = () => {
   const { state } = useLocation();
   const { query } = state;
   const [listings, setListings] = useState([]);
+  const [bidListings, setBidListings] = useState([]);
+  const navigate = useNavigate();
+
   async function getListings() {
     try {
       const { data } = await axios.post('/item/search', {
@@ -22,38 +29,118 @@ const SearchResults = () => {
     }
   }
 
+  async function getBidListings() {
+    try {
+      const { data } = await axios.post('/item/bidSearch', {
+        filter: query,
+      });
+      if (data && data.length > 0) {
+        setBidListings(data.reverse());
+      }
+    } catch (error) {
+      throw new Error('Error searching for items');
+    }
+  }
+
   useEffect(() => {
     getListings();
+    getBidListings();
   }, []);
 
-  const rows = Math.ceil(listings.length / 3);
-  const items = (Array(rows).fill().map((_, rowIndex) => (
-    <Row>
-      {
-        listings.slice(rowIndex * 3, (rowIndex * 3) + 3).map((result) => (
-          <Col>
-            <ResultCard
-              key={result.id}
-              title={result.itemName}
-              price={result.price}
-              lister={result.posterName}
-              tag={result.tag}
-              image={result.media}
-            />
-          </Col>
-        ))
-      }
-    </Row>
-  )));
+  // const rows = Math.ceil(listings.length / 3);
+  // const items = (Array(rows).fill().map((_, rowIndex) => (
+  //   <Row>
+  //     {
+  //       listings.slice(rowIndex * 3, (rowIndex * 3) + 3).map((result) => (
+  //         <Col>
+  //           <ResultCard
+  //             /* eslint no-underscore-dangle: 0 */
+  //             key={result._id}
+  //             title={result.itemName}
+  //             price={result.price}
+  //             lister={result.posterName}
+  //             tag={result.tag}
+  //             image={result.media}
+  //           />
+  //         </Col>
+  //       ))
+  //     }
+  //   </Row>
+  // )));
 
   return (
-    <div style={{
-      margin: '30px', alignContent: 'center', alignSelf: 'center', alignItems: 'center', justifyContent: 'center', justifyItems: 'center',
-    }}
-    >
-      <h4 style={{ textAlign: 'left' }}>{`${listings.length} results for ${query}`}</h4>
-      {items}
-      <SearchPagination />
+    <div className="homepage pt-5">
+      <div className="carousel-wrapper">
+        <h4 style={{ marginTop: '0 auto' }}> {listings.length} Regular listing results for &quot;{query}&quot;</h4>
+        <CarouselProvider
+          className="mt-1"
+          naturalSlideWidth={100}
+          naturalSlideHeight={100}
+          totalSlides={listings.length}
+          visibleSlides={5}
+        >
+          <Slider>
+            {listings.map((item, idx) => (
+              // eslint-disable-next-line no-underscore-dangle
+              <Slide onClick={() => navigate('/item', { state: { itemId: item._id } })} index={idx} key={uuidv4()}>
+                {item.media && item.media !== ''
+                  ? (
+                    <>
+                      <Image src={item.media} alt="product pic" hasMasterSpinner={false} />
+                      <p style={{ width: '100%' }}><b>{`$${item.price}`}</b>{`, listed by ${item.posterName.split(' ')[0]}`}</p>
+                    </>
+                  )
+                  : (
+                    <div className="d-flex flex-column justify-content-center align-items-center" style={{ height: '100%' }}>
+                      <h1>{item.itemName}</h1>
+                      <p style={{ width: '100%', textAlign: 'center' }}><b>{`Price: $${item.price}`}</b>{`, listed by ${item.posterName.split(' ')[0]}`}</p>
+                    </div>
+                  )}
+              </Slide>
+            ))}
+          </Slider>
+          <div className="buttons">
+            <ButtonBack className="btn me-2">Back</ButtonBack>
+            <ButtonNext className="btn">Next</ButtonNext>
+          </div>
+        </CarouselProvider>
+      </div>
+
+      <div className="carousel-wrapper">
+        <h4> {bidListings.length} Bid listing results for &quot;{query}&quot;</h4>
+        <CarouselProvider
+          className="mt-1"
+          naturalSlideWidth={100}
+          naturalSlideHeight={100}
+          totalSlides={bidListings.length}
+          visibleSlides={5}
+        >
+          <Slider>
+            {bidListings.map((item, idx) => (
+              // eslint-disable-next-line no-underscore-dangle
+              <Slide onClick={() => navigate('/item', { state: { itemId: item._id } })} index={idx} key={uuidv4()}>
+                {item.media && item.media !== ''
+                  ? (
+                    <>
+                      <Image src={item.media} alt="product pic" hasMasterSpinner={false} />
+                      <p style={{ width: '100%' }}><b>{`Highest Bid: $${item.price}`}</b>{`, listed by ${item.posterName.split(' ')[0]}`}</p>
+                    </>
+                  )
+                  : (
+                    <div className="d-flex flex-column justify-content-center align-items-center" style={{ height: '100%' }}>
+                      <h1>{item.itemName}</h1>
+                      <p style={{ width: '100%', textAlign: 'center' }}><b>{`$${item.price}`}</b>{`, listed by ${item.posterName.split(' ')[0]}`}</p>
+                    </div>
+                  )}
+              </Slide>
+            ))}
+          </Slider>
+          <div className="buttons">
+            <ButtonBack className="btn me-2">Back</ButtonBack>
+            <ButtonNext className="btn">Next</ButtonNext>
+          </div>
+        </CarouselProvider>
+      </div>
     </div>
   );
 };

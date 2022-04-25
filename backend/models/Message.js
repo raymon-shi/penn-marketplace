ac077121a0bde72a9e93133ea6e88ae5f7dd11a2
Raymon Shi <rshi.raymon@gmail.com>
2022-04-24 22:17:24 -0400

    added images to messages

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -6,6 +6,7 @@ const MessageSchema = new Schema({
   sender: { type: String, required: true },
   receiver: { type: String, required: true },
   message: { type: String, required: true },
+  img: { type: String },
 }, { timestamps: true });
 
 const Message = model('Message', MessageSchema);

09dadf8816800d32ce9c555dc2498c3b14da6072
damon-luong <ldamon@sas.upenn.edu>
2022-05-07 00:49:59 -0400

    added buyer error tests
    added buyer error tests

--- frontend/src/buyer/tests/api.test.js
+++ frontend/src/buyer/tests/api.test.js
@@ -70,10 +70,20 @@ test('/buyer/getRegListing/:id', async () => {
   expect(response).toMatchObject(regListing);
 });
 
+test('/buyer/getRegListing/:id throws error', async () => {
+  mock.onGet(`/buyer/getRegListing/${bidListing._id}`).networkError();
+  expect(api.getRegListing(bidListing._id)).rejects.toThrowError();
+});
+
 test('/buyer/getBidListing:id', async () => {
   mock.onGet(`/buyer/getBidListing/${bidListing._id}`).reply(200, bidListing);
   const response = await api.getBidListing(bidListing._id);
   expect(response).toMatchObject(bidListing);
+});
+
+test('/buyer/getBidListing/:id throws error', async () => {
+  mock.onGet(`/buyer/getBidListing/${regListing._id}`).networkError();
+  expect(api.getBidListing(regListing._id)).rejects.toThrowError();
 });
 
 test('/buyer/cart', async () => {
@@ -82,10 +92,20 @@ test('/buyer/cart', async () => {
   expect(response).toMatchObject(cart);
 });
 
+test('/buyer/cart throws error', async () => {
+  mock.onGet('/buyer/cart').networkError();
+  expect(api.getCart).rejects.toThrowError();
+});
+
 test('/buyer/addCartRegItem/:id', async () => {
   mock.onPost(`/buyer/addCartRegItem/${regListing._id}`).reply(200, cart);
   const response = await api.addCart(regListing._id);
   expect(response).toMatchObject(cart);
+});
+
+test('/buyer/addCartRegItem/:id throws error', async () => {
+  mock.onPost(`/buyer/addCartRegItem`).networkError();
+  expect(api.addCart()).rejects.toThrowError();
 });
 
 test('/buyer/addWatchRegItem/:id', async () => {
@@ -94,10 +114,20 @@ test('/buyer/addWatchRegItem/:id', async () => {
   expect(response).toMatchObject(savedReg);
 });
 
+test('/buyer/addWatchRegItem/:id throws error', async () => {
+  mock.onPost(`/buyer/addWatchRegItem`).networkError();
+  expect(api.saveReg()).rejects.toThrowError();
+});
+
 test('/buyer/addWatchBidItem/:id', async () => {
   mock.onPost(`/buyer/addWatchBidItem/${bidListing._id}`).reply(200, savedbid);
   const response = await api.saveBid(bidListing._id);
   expect(response).toMatchObject(savedbid);
+});
+
+test('/buyer/addWatchBidItem/:id throws error', async () => {
+  mock.onPost(`/buyer/addWatchBidItem`).networkError();
+  expect(api.saveBid()).rejects.toThrowError();
 });
 
 test('/buyer/removeCartRegItem/:id', async () => {
@@ -106,10 +136,20 @@ test('/buyer/removeCartRegItem/:id', async () => {
   expect(response).toMatchObject({ shoppingCart: [] });
 });
 
+test('/buyer/removeCartRegItem/:id throws error', async () => {
+  mock.onPost(`/buyer/removeCartRegItem`).networkError();
+  expect(api.handleRemoveReg()).rejects.toThrowError();
+});
+
 test('/buyer/addBid/:id', async () => {
   mock.onPost(`/buyer/addBid/${bidListing._id}`).reply(200, updatedBidListing);
   const response = await api.addBid(bidListing._id, 51);
   expect(response).toMatchObject(updatedBidListing);
+});
+
+test('/buyer/addBid/:id throws error', async () => {
+  mock.onPost(`/buyer/addBid`).networkError();
+  expect(api.addBid()).rejects.toThrowError();
 });
 
 test('/seller/acceptBid', async () => {
@@ -118,14 +158,29 @@ test('/seller/acceptBid', async () => {
   expect(response).toMatchObject(updatedBidListing);
 });
 
+test('/buyer/acceptBid throws error', async () => {
+  mock.onPost(`/buyer/acceptBid`).networkError();
+  expect(api.acceptBid()).rejects.toThrowError();
+});
+
 test('/buyer/regTransaction', async () => {
   mock.onPost('/buyer/regTransaction').reply(200, transaction);
   const response = await api.regTransaction(regListing, info);
   expect(response).toMatchObject(transaction);
 });
 
+test('/buyer/regTransaction throws error', async () => {
+  mock.onPost(`/buyer/regTransaction`).networkError();
+  expect(api.regTransaction()).rejects.toThrowError();
+});
+
 test('/seller/addTransaction', async () => {
   mock.onPost('/seller/addTransaction').reply(200, transaction);
   const response = await api.addTransaction(transaction);
   expect(response).toMatchObject(transaction);
+});
+
+test('/buyer/addTransaction throws error', async () => {
+  mock.onPost(`/seller/addTransaction`).networkError();
+  expect(api.addTransaction()).rejects.toThrowError();
 });
